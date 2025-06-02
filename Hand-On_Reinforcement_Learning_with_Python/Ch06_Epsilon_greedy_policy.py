afb0309afe9ec8b18de9317edbb74207031aba49
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2025-06-01 20:35:36 -0400

    Create Ch06_Epsilon_greedy_policy.py

---
+++ Hand-On_Reinforcement_Learning_with_Python/Ch06_Epsilon_greedy_policy.py
@@ -0,0 +1,51 @@
+# 必須要先安裝吃角子老虎機的環境
+# git clone https://github.com/JKCooper2/gym-bandits.git
+# cd gym-bandits
+# pip install -e
+
+import gym_bandits
+import gym
+import numpy as np
+import math
+import random
+
+# 初始化環境，設定有十隻手臂的 MAB
+env = gym.make("BanditTenArmedGaussian-v0")
+
+print(env.action_space)
+
+# Epsilon greedy policy
+
+# 回合次數 (遞迴)
+num_rounds = 20000
+
+# 某隻手臂被拉下幾次的計數器
+# 有十隻手臂，全部都先初始化為 0
+count = np.zeros(10)
+
+# 各手臂的獎勵總和, 每隻手臂分別計算自己的獎勵總和
+sum_rewards = np.zeros(10)
+
+# Q 值, 也就是平均獎勵, 這也是每隻手臂分別計算
+Q = np.zeros(10)
+
+# 定義函數
+def epsilon_greedy(epsilon):
+    rand = np.random.random()
+    if rand < epsilon:
+        # 機率比 epsilon 小的時候, 隨機選擇要拉哪一隻手臂
+        action = env.action_space.sample()
+    else:
+        # 機率 >= epsilon 的時候, 拉 Q 值最大的那隻手臂
+        action = np.argmax(Q)
+    return action
+
+# 開始拉手臂
+for i in range(num_rounds):
+    arm = epsilon_greedy(0.5)
+    observation, reward, done, info = env.step(arm)
+    count[arm] += 1
+    sum_rewards[arm] += reward
+    Q[arm] = sum_rewards[arm] / count[arm]
+
+print("The optimal arm is {}".format(np.argmax(Q)))
